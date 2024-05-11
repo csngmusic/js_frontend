2a9d4776028eaeb78c218c7b1c3ff8f43e7fe1b1
Gregory Kuzin <g.kuzin@dssl.ru>
2024-05-11 12:20:42 +0300

    17tutor

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -2,49 +2,14 @@ import React from 'react';
 import ToDoTask from './ToDoTask';
 
 class ToDoList extends React.Component{
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      tasks: []
-    }
-
-    this.onTaskDelete = this.onTaskDelete.bind(this);
-    this.onTaskAdd = this.onTaskAdd.bind(this);
-  }
-
-  componentDidMount() {
-    fetch('tasks').then(function(res) {
-      return res.json();
-    }).then((data) => {
-      this.setState({
-        tasks: data
-      });
-    });
-  }
-
-  onTaskDelete(_id) {
-    this.setState({
-      tasks: this.state.tasks.filter(function(task) {
-        return task._id !== _id;
-      })
-    });
-  }
-
-  onTaskAdd(task) {
-    this.setState({
-      tasks: [...this.state.tasks, task]
-    });
-  }
-  
   render() {
     return (
       <div className="List">
         <ul>
           {
-            this.state.tasks.map((task) => {
+            this.props.tasks.map((task) => {
               return(
-                <ToDoTask task={task} onTaskDelete = {this.onTaskDelete} key = {task._id} />
+                <ToDoTask task={task} onTaskDelete = {this.props.onTaskDelete} key = {task._id} />
               )
             })
           }
--- src/ToDoTaskAdd.js
+++ src/ToDoTaskAdd.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-class ToDoTaskAdd extends React.Component{
+class ToDoTaskAddInner extends React.Component{
   constructor(props) {
     super(props);
 
@@ -45,6 +46,7 @@ class ToDoTaskAdd extends React.Component{
       return res.json();
     }).then((data) => {
       this.props.onTaskAdd(data);
+      this.props.history('/');
     });
   }
   
@@ -58,5 +60,9 @@ class ToDoTaskAdd extends React.Component{
     )
   }
 }
-
+const ToDoTaskAdd = (props) => {
+  return (
+    <ToDoTaskAddInner {...props} history={useNavigate()} />
+  )
+}
 export default ToDoTaskAdd;
